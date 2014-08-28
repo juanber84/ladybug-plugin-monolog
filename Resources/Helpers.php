055f192f9276fabd81d4189987b5a4e6b386612b
Juanber <juanber84@gmail.com>
2014-08-28 23:47:39 +0200

    Add monolog for write in log the ladybug output

--- Resources/Helpers.php
+++ Resources/Helpers.php
@@ -2,10 +2,24 @@
 
 // helpers
 
+/**
+ * Gets the logger
+ *
+ * @return Monolog\Logger
+ */
+function getMonolog()
+{
+    $monolog = new \Monolog\Logger('logger');
+    return $monolog;
+}
+
 function ladybug_logger_dump(/*$var1 [, $var2...$varN]*/)
 {
     $ladybug = getLadybug();
     ladybug_set_format('text');
-    echo call_user_func_array(array($ladybug,'dump'), func_get_args());
+    $r = call_user_func_array(array($ladybug,'dump'), func_get_args());
+    $log = getMonolog();
+    $log->pushHandler(new \Monolog\Handler\StreamHandler('logs/prueba.log'));
+    $log->debug($r);
     ladybug_set_format('html');
 }
