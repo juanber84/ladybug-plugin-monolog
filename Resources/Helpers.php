381f472e51fcae7ee3ceb254647f5624d0b12940
Juanber <juanber84@gmail.com>
2014-08-28 22:10:00 +0200

    Remove method

--- Resources/Helpers.php
+++ Resources/Helpers.php
@@ -1,24 +1,6 @@
 <?php
 
-
-
 // helpers
-
-/**
- * Gets the ladybug dumper
- *
- * @return Ladybug\Dumper
- */
-function getLadybug()
-{
-    global $ladybug;
-
-    if (is_null($ladybug)) {
-        $ladybug = new \Ladybug\Dumper();
-    }
-
-    return $ladybug;
-}
 
 function ladybug_logger_dump(/*$var1 [, $var2...$varN]*/)
 {
