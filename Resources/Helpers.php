cad4f02f70d6fe35abc9d2fb9d0181520530636d
Juanber <juanber84@gmail.com>
2014-08-28 22:22:46 +0200

    Add set and restore format in custom helper

--- Resources/Helpers.php
+++ Resources/Helpers.php
@@ -5,5 +5,7 @@
 function ladybug_logger_dump(/*$var1 [, $var2...$varN]*/)
 {
     $ladybug = getLadybug();
+    ladybug_set_format('text');
     echo call_user_func_array(array($ladybug,'dump'), func_get_args());
+    ladybug_set_format('html');
 }
